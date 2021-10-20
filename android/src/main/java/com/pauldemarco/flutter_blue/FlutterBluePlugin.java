e6add253d51c554c96de226327bbb1b35df6a456
AndreaMinato <andrea.minato@hotmail.it>
2021-10-20 10:52:51 +0200

    null check on channel invoke and syncronized teardown

--- android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
@@ -65,6 +65,7 @@ public class FlutterBluePlugin implements FlutterPlugin, ActivityAware, MethodCa
     private static final String TAG = "FlutterBluePlugin";
     private Object initializationLock = new Object();
     private Context context;
+    private Object tearDownLock = new Object();
     private MethodChannel channel;
     private static final String NAMESPACE = "plugins.pauldemarco.com/flutter_blue";
 
@@ -166,17 +167,19 @@ public class FlutterBluePlugin implements FlutterPlugin, ActivityAware, MethodCa
     }
 
     private void tearDown() {
-        Log.i(TAG, "teardown");
-        context = null;
-        activityBinding.removeRequestPermissionsResultListener(this);
-        activityBinding = null;
-        channel.setMethodCallHandler(null);
-        channel = null;
-        stateChannel.setStreamHandler(null);
-        stateChannel = null;
-        mBluetoothAdapter = null;
-        mBluetoothManager = null;
-        application = null;
+        synchronized (tearDownLock) {
+            Log.i(TAG, "teardown");
+            context = null;
+            activityBinding.removeRequestPermissionsResultListener(this);
+            activityBinding = null;
+            channel.setMethodCallHandler(null);
+            channel = null;
+            stateChannel.setStreamHandler(null);
+            stateChannel = null;
+            mBluetoothAdapter = null;
+            mBluetoothManager = null;
+            application = null;
+        }
     }
 
 
@@ -999,7 +1002,11 @@ public class FlutterBluePlugin implements FlutterPlugin, ActivityAware, MethodCa
                 new Runnable() {
                     @Override
                     public void run() {
-                        channel.invokeMethod(name, byteArray);
+                        synchronized (tearDownLock) {
+                            if (channel != null) {
+                                channel.invokeMethod(name, byteArray);
+                            }
+                        }
                     }
                 });
     }
